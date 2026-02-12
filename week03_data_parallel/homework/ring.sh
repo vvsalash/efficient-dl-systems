204aa1823867a3f2f672fa50993045f1d2267c10
vvsalash <vova.salash609@gmail.com>
2026-02-12 13:01:05 +0300

    Add task4 in hw2

--- week03_data_parallel/homework/ring.sh
+++ week03_data_parallel/homework/ring.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 for w in 1 2 4 8 16 32; do
-  python allreduce.py --impl ring --world_size $w --sweep --check
+  python allreduce.py --impl ring --world_size $w --sweep --numels $w --check --iters 200 --warmup 20
 done
 
